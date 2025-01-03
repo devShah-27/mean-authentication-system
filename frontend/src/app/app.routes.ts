7c34e09756e5c65505dfca5a090efab48228e595
Dev Shah <shahdev149@gmail.com>
2025-01-03 22:08:28 +0530

    Move AuthGuard to its own folder

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
-import { authGuard } from './auth/auth.guard';
+import { authGuard } from './guards/auth.guard';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 export const routes: Routes = [
