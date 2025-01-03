3fcbdae85c3b7cdd04a28ebdfb152f8146aac6e4
Dev Shah <shahdev149@gmail.com>
2025-01-04 00:00:18 +0530

    Added route to request a reset

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { ResetPasswordComponent } from './auth/reset-password/reset-password.component';
+import { RequestResetComponent } from './auth/request-reset/request-reset.component';
 import { authGuard } from './guards/auth.guard';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
@@ -9,10 +10,11 @@ export const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  { path: 'request-reset', component: RequestResetComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
   {
     path: 'dashboard',
     component: DashboardComponent,
     canActivate: [authGuard],
-  }, // Protect the dashboard route
+  },
 ];
